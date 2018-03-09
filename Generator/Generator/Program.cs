ae6a80c31c4a0b82762b165cfbb8efdc1c27b37e
Nate C-K <ncmadcity@gmail.com>
2018-03-09 13:35:28 -0600

    Move Generator solution.

--- Generator/Generator/Program.cs
+++ Generator/Generator/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.IO;
 
 namespace Generator
 {
     class Program
     {
-        static Size MapSize = new Size(100, 100);
+        static Size MapSize = new Size(10, 10);
         static Size TileSize = new Size(100, 100);
         static void Main(string[] args)
         {
+            string dir = (args.Length > 0 ? args[0] : ".");
+            Directory.SetCurrentDirectory(dir);
             var map = Generator.NewMap(MapSize, TileSize);
             map.Save("map");
         }
