793e6d513b4ec6a6060b2b031123a077a1bd336d
Nate C-K <ncmadcity@gmail.com>
2018-03-10 10:32:39 -0600

    Fix background drawing.

--- c/wandrix.c
+++ c/wandrix.c
@@ -9,33 +9,21 @@
 const char* WINDOW_NAME = "Wandrix";
 const int SCREEN_W = 800;
 const int SCREEN_H = 600;
+const int FULLSCREEN = 0;
+const int VSYNC = 1;
 const Uint32 LOGIC_FRAMES_PER_SEC = 16; // fixed rate
 const Uint32 RENDER_FRAMES_PER_SEC = 32; // rate cap
 
 struct Coords { int x, y; };
 struct Size { int w, h; };
 
-struct Coords Coords(int x, int y)
-{
-  struct Coords c;
-  c.x = x;
-  c.y = y;
-  return c;
-}
-
-struct Size Size(int w, int h)
-{
-  struct Size s;
-  s.w = w;
-  s.h = h;
-  return s;
-}
-
 SDL_Window* window;
 SDL_Renderer* renderer;
 SDL_Surface* screen;
 SDL_Texture* map;
+struct Coords center;
 struct Size maxTextureSize;
+struct Size mapImageSize;
 int quitting = 0;
 struct Coords playerPos;
 struct Coords playerMove;
@@ -62,15 +50,16 @@ int init()
     fprintf(stderr, "SDL_image init failed: %s\n", IMG_GetError() );
     return 0;
   }
-  window = SDL_CreateWindow(WINDOW_NAME,
-      SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-      SCREEN_W, SCREEN_H, SDL_WINDOW_SHOWN);
+  Uint32 windowFlags = (FULLSCREEN * SDL_WINDOW_FULLSCREEN_DESKTOP);
+  int wPos = SDL_WINDOWPOS_CENTERED;
+  window = SDL_CreateWindow(WINDOW_NAME, wPos, wPos, SCREEN_W, SCREEN_H, windowFlags);
   if (!window)
   {
     fprintf(stderr, "Create window failed: %s\n", SDL_GetError());
     return 0;
   }
-  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  renderer = SDL_CreateRenderer(window, -1,
+      SDL_RENDERER_ACCELERATED | (VSYNC * SDL_RENDERER_PRESENTVSYNC));
   if (!renderer)
   {
     fprintf(stderr, "Create renderer failed: %s\n", SDL_GetError());
@@ -86,6 +75,8 @@ int init()
   maxTextureSize.h = rendererInfo.max_texture_height;
   printf("Max texture size: %dx%d\n", maxTextureSize.w, maxTextureSize.h);
   screen = SDL_GetWindowSurface(window);
+  center.x = screen->w / 2;
+  center.y = screen->h / 2;
   atexit(atExitHandler);
   return 1;
 }
@@ -124,6 +115,9 @@ int loadAssets()
 {
   map = loadImageAsTexture("map.png");
   if (!map) return 0;
+  Uint32 format;
+  int access;
+  SDL_QueryTexture(map, &format, &access, &mapImageSize.w, &mapImageSize.h);
   return 1;
 }
 
@@ -138,9 +132,9 @@ struct Coords scanMoveKeys()
   if (currentKeyStates[SDL_SCANCODE_LEFT])
     move.x -= 1;
   if (currentKeyStates[SDL_SCANCODE_RIGHT])
-    move.x -= 1;
-  if (currentKeyStates[SDL_SCANCODE_Q])
-    quitting = 1;
+    move.x += 1;
+  //if (currentKeyStates[SDL_SCANCODE_Q])
+  //  quitting = 1;
   return move;
 }
 
@@ -162,8 +156,8 @@ end
 int updateLogic()
 {
   // Apply previous move.
-  playerPos.x += playerMove.x;
-  playerPos.y += playerMove.y;
+  playerPos.x += playerMove.x * 8;
+  playerPos.y += playerMove.y * 8;
   // Get next move. (We need it now to interpolate.)
   playerMove = scanMoveKeys();
   // TODO: Cancel move if invalid.
@@ -174,11 +168,38 @@ int updateDisplay(Uint32 phase)
   // TODO: Interpolate object positions.
 }
 
+int drawTexture(SDL_Rect* screenRect, SDL_Texture* texture, SDL_Rect* textureRect)
+{
+  SDL_Rect intersectRect;
+  int intersects =
+    (SDL_TRUE == SDL_IntersectRect(screenRect, textureRect, &intersectRect));
+  if (intersects) {
+    SDL_Rect screenDestRect = {
+      intersectRect.x - screenRect->x, intersectRect.y - screenRect->y,
+      intersectRect.w, intersectRect.h };
+    SDL_RenderCopy(renderer, texture, &intersectRect, &screenDestRect);
+  }
+  return intersects;
+}
+
 int draw()
 {
+  SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
   SDL_RenderClear(renderer);
-  SDL_RenderCopy(renderer, map, NULL, NULL);
+  SDL_Rect screenRect = {
+    playerPos.x - center.x, playerPos.y - center.y,
+    screen->w, screen->h };
+  SDL_Rect wholeMapRect = { 0, 0, mapImageSize.w, mapImageSize.h };
+  drawTexture(&screenRect, map, &wholeMapRect);
   SDL_RenderPresent(renderer);
+}
+
+void handleKeypress(SDL_Event* e)
+{
+  switch (e->key.keysym.sym) {
+    // case SDLK_UP:
+    case SDLK_q: quitting = 1; break;
+  }
 }
 
 void pollEvents()
@@ -188,15 +209,19 @@ void pollEvents()
     if (e.type == SDL_QUIT) {
       quitting = 1;
     }
+    if (e.type == SDL_KEYDOWN) {
+      handleKeypress(&e);
+    }
   }
 }
 
 int mainLoop()
 {
-  Uint32 startTime = SDL_GetTicks();
-  Uint32 nextLogicFrameTime = 0, nextRenderFrame = 0;
-  Uint32 logicFrameTimeMs = 1000 / LOGIC_FRAMES_PER_SEC;
-  Uint32 renderFrameTimeMs = 1000 / RENDER_FRAMES_PER_SEC;
+  Uint32 startTime = SDL_GetTicks(),
+         nextLogicFrameTime = 0,
+         nextRenderFrame = 0,
+         logicFrameTimeMs = 1000 / LOGIC_FRAMES_PER_SEC,
+         renderFrameTimeMs = 1000 / RENDER_FRAMES_PER_SEC;
   while (!quitting)
   {
     // TODO: Reset frame times once per second to prevent rounding
@@ -205,19 +230,17 @@ int mainLoop()
     pollEvents();
     if (nextLogicFrameTime > time)
       SDL_Delay(0); // Be a little nice with the CPU when ahead of schedule.
-    while (nextLogicFrameTime < time)
-    {
+    while (nextLogicFrameTime < time) {
       nextLogicFrameTime += logicFrameTimeMs;
       updateLogic();
     }
+    //int skipDraw = 0;
     int skipDraw = 1;
-    while (nextRenderFrame < time)
-    {
+    while (nextRenderFrame < time) {
       nextRenderFrame += renderFrameTimeMs;
       skipDraw = 0;
     }
-    if (!skipDraw)
-    {
+    if (!skipDraw) {
       Uint32 phase = 1000 - (nextLogicFrameTime - time) * 1000 / LOGIC_FRAMES_PER_SEC;
       updateLogic(phase);
       draw();
